af1cc03caa4e13c3d8f5388439eeec7520f7f1c0
e233qq <e233qq@gmail.com>
2021-12-22 19:28:05 +0800

    feat: add files

---
+++ src/index.js
@@ -0,0 +1,59 @@
+const { validateMetadata } = require("/opt/verdaccio/build/lib/utils");
+const bodyParser = require("body-parser");
+
+function Plugin(config, pluginOptions) {
+  const self = Object.create(Plugin.prototype);
+
+  self._config = config;
+  self.permission = self._config.permission;
+  self._logger = pluginOptions.logger;
+  return self;
+}
+
+Plugin.prototype.register_middlewares = function (app, auth) {
+  if (!this._config.enabled) {
+    return;
+  }
+  const logger = this._logger;
+  const permission = this.permission;
+  app.put(
+    "/:package/:_rev?/:revision?",
+    bodyParser.json({ strict: false, limit: "10mb" }),
+    auth.apiJWTmiddleware(),
+    function (req, res, next) {
+      if (req.headers.referer !== "publish") {
+        return next();
+      }
+      const packageName = req.params.package;
+      if (packageName === "-") {
+        return next();
+      }
+      const metadata = validateMetadata(req.body, packageName);
+      const remote = req.remote_user;
+
+      let rejected = false;
+      if (permission[packageName] && metadata["dist-tags"]) {
+        Object.keys(metadata["dist-tags"]).forEach((tag) => {
+          if (!permission[packageName][tag]) {
+            return;
+          }
+
+          if (!permission[packageName][tag].includes(remote.name)) {
+            logger.info(
+              `rejected ${remote.name} publish latest tag of ${metadata.name}`
+            );
+            rejected = true;
+          }
+        });
+      }
+
+      if (rejected) {
+        res.status(403).end();
+        return;
+      }
+      next();
+    }
+  );
+};
+
+module.exports = Plugin;
